0afc35954f7ddf994fdfee1f87fce213f1f73221
kh <kitt.hsu@gmail.com>
2020-05-09 22:58:07 +0800

    update pkg/sidecar/api.go|delta(566)

--- pkg/sidecar/api.go
+++ pkg/sidecar/api.go
@@ -1,5 +1,39 @@
 package sidecar
 
-func Run(name string, arg ...string) {
+import (
+	"context"
+	"os/exec"
+)
 
+type chanWriter []chan string
+
+func (w chanWriter) Write(p []byte) (n int, err error) {
+	for _, c := range w{
+		c <- string(p)
+	}
+
+	n = len(p)
+	return
+}
+
+type Runner struct {
+	stdout chanWriter
+	stderr chanWriter
+	ctx context.Context
+	Kill context.CancelFunc
+}
+
+type RunnerStdout struct {
+
+}
+
+func New(name string, arg ...string) (r *Runner) {
+	r = &Runner{}
+
+	r.ctx, r.Kill = context.WithCancel(context.Background())
+
+	gitProcess := exec.CommandContext(r.ctx, name, arg...)
+	gitProcess.Stdout = r.stdout
+	gitProcess.Stderr = r.stderr
+	return
 }
