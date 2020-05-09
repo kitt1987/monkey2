4bb6f647e4107cc3c73ced9335f7b7ed32c6ae97
kh <kitt.hsu@gmail.com>
2020-05-09 23:11:58 +0800

    update pkg/sidecar/api.go|delta(850)

--- pkg/sidecar/api.go
+++ pkg/sidecar/api.go
@@ -16,24 +16,36 @@ func (w chanWriter) Write(p []byte) (n int, err error) {
 	return
 }
 
+func New(name string, arg ...string) (r *Runner) {
+	r = &Runner{}
+
+	r.ctx, r.Kill = context.WithCancel(context.Background())
+	r.proc = exec.CommandContext(r.ctx, name, arg...)
+	return
+}
+
 type Runner struct {
 	stdout chanWriter
 	stderr chanWriter
 	ctx context.Context
 	Kill context.CancelFunc
+	proc *exec.Cmd
 }
 
-type RunnerStdout struct {
-
+func (r *Runner) Start() error {
+	r.proc.Stdout = r.stdout
+	r.proc.Stderr = r.stderr
+	return r.proc.Start()
 }
 
-func New(name string, arg ...string) (r *Runner) {
-	r = &Runner{}
+func (r *Runner) Stdout() <-chan string {
+	ch := make(chan string)
+	r.stdout = append(r.stdout, ch)
+	return ch
+}
 
-	r.ctx, r.Kill = context.WithCancel(context.Background())
-
-	gitProcess := exec.CommandContext(r.ctx, name, arg...)
-	gitProcess.Stdout = r.stdout
-	gitProcess.Stderr = r.stderr
-	return
+func (r *Runner) Stderr() <-chan string {
+	ch := make(chan string)
+	r.stderr = append(r.stderr, ch)
+	return ch
 }
